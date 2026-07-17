70ab777cf58acd2d23a7bfc00517f481e212b78c
anjolaa814 <anjolaa814@gmail.com>
2026-07-17 12:05:49 -0400

    Update course.h

--- course.h
+++ course.h
@@ -3,4 +3,10 @@ using namesapce std;
 
 class Course { 
   public:
-      string getCourseName;
+      string getCourseName();
+      string getCourseId();
+  private:
+      string courseName;
+      string courseid;
+      
+
