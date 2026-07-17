3dd55703831f27dbeb3fefcef76694940955dd5f
anjolaa814 <anjolaa814@gmail.com>
2026-07-17 12:15:51 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int role;
+  cout << "Choose a role" << endl;
+  cin >> role;
+  // if user enters 1, Student. If user enters 2, Admin; If user enters 3, exit.
+  if(role < 0  && role > 3)
+  {  
+    cout << "Invalid, Choose a role" << endl;
+  }
+    
 
