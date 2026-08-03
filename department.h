d3518141fb83d048c4d82b7234412502260dc54a
Aldray Anderson <164082933+aandersonob@users.noreply.github.com>
2026-08-03 19:30:50 -0400

    Add Department class definition with methods

--- department.h
+++ department.h
@@ -1,1 +1,27 @@
-
+#ifndef DEPARTMENT_H
+#define DEPARTMENT_H
+#include "Course.h"
+ 
+class Department {
+private:
+    char    name[50];
+    Course* courses;
+    int     courseCount;
+ 
+public:
+ 
+    Department();
+    Department(const char* deptName);
+ 
+    Department(const Department& src);
+    Department& operator=(const Department& src);
+    Department();
+ 
+    void addCourse(const Course& c);
+    const char* getName() const;
+    int  getCourseCount() const;
+    Course getCourse(int index) const;
+};
+ 
+#endif
+ 
