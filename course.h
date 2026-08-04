75f5e69741095a47754dd286f6750f4af6aed530
Aldray Anderson <164082933+aandersonob@users.noreply.github.com>
2026-08-04 12:58:59 -0400

    Refactor Course class with new attributes and methods

--- course.h
+++ course.h
@@ -1,13 +1,22 @@
-# include <iostream>
-using namesapce std;
-
-class Course { 
-  public:
-      string getCourseName();
-      string getCourseId();
-  private:
-      string courseName;
-      string courseid;
-
-      
-
+#pragma once
+#include <string>
+using namespace std;
+ 
+class Course
+{
+private:
+    string number;
+    string name;
+    string schedule;
+    double price;
+ 
+public:
+    Course();
+    Course(const string& number, const string& name, const string& schedule, double price);
+ 
+    string getNumber() const;
+    string getName() const;
+    string getSchedule() const;
+    double getPrice() const;
+};
+ 
