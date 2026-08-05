028c692a8c70d8c05bae413a10a0fd2dd3491efc
Aldray Anderson <164082933+aandersonob@users.noreply.github.com>
2026-08-05 15:37:11 -0400

    Add StudentInterface class with cart functionality

--- Studentinterface.h
+++ Studentinterface.h
@@ -1,1 +1,16 @@
+#pragma once
+#include "Interface.h"
+#include "Cart.h"
 
+class StudentInterface : public Interface
+{
+private:
+    Cart cart;
+
+    void showCartMenu();
+    void browseDepartments();
+    void listDepartmentCourses(int index);
+
+public:
+    void run() override;
+};
